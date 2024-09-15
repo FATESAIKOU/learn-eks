1d3e450c341492ac4092012c043bd7945bf7c83c
FATESAIKOU <qzsecftbhhhh@gmail.com>
2024-09-15 14:10:26 +0900

    Add fluxcd install/uninstall script

---
+++ controller/install-fluxcd.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+kubectl apply -f https://github.com/fluxcd/flux2/releases/latest/download/install.yaml
---
+++ controller/uninstall-fluxcd.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+kubectl delete -f https://github.com/fluxcd/flux2/releases/latest/download/install.yaml
